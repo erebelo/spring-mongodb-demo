1e2245c2644150977ef055e95b54b6711b958895
erebelo <eduldrebelo@gmail.com>
2023-02-04 13:12:08 -0300

    Removing parenthesis from CommonErrorCodesEnum

--- src/main/java/com/erebelo/springmongodbdemo/exception/CommonErrorCodesEnum.java
+++ src/main/java/com/erebelo/springmongodbdemo/exception/CommonErrorCodesEnum.java
@@ -2,16 +2,15 @@ package com.erebelo.springmongodbdemo.exception;
 
 public enum CommonErrorCodesEnum implements ErrorCode {
 
-    // TODO try removing parentheses
-    COMMON_ERROR_400_000(),
-    COMMON_ERROR_401_000(),
-    COMMON_ERROR_422_000(),
-    COMMON_ERROR_500_000(),
-    COMMON_ERROR_404_001(),
-    COMMON_ERROR_404_002(),
-    COMMON_ERROR_404_003(),
-    COMMON_ERROR_404_004(),
-    COMMON_ERROR_409_001();
+    COMMON_ERROR_400_000,
+    COMMON_ERROR_401_000,
+    COMMON_ERROR_422_000,
+    COMMON_ERROR_500_000,
+    COMMON_ERROR_404_001,
+    COMMON_ERROR_404_002,
+    COMMON_ERROR_404_003,
+    COMMON_ERROR_404_004,
+    COMMON_ERROR_409_001;
 
     private final String propertyKey;
 
