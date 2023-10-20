c481c4863d8252b17b390ea930c2656b00abebfb
erebelo <eduldrebelo@gmail.com>
2023-10-20 20:00:18 -0300

    Rounding up BigDecimal values

--- src/main/java/com/erebelo/springmongodbdemo/mapper/ProfileMapper.java
+++ src/main/java/com/erebelo/springmongodbdemo/mapper/ProfileMapper.java
@@ -9,6 +9,9 @@ import com.erebelo.springmongodbdemo.domain.response.ProfileResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static java.util.Objects.isNull;
 import static org.mapstruct.ReportingPolicy.WARN;
 
@@ -17,12 +20,22 @@ public interface ProfileMapper {
 
     ProfileResponse entityToResponse(UserProfile entity);
 
+    @Mapping(target = "estimatedAnnualIncome", expression = "java(roundUpBigDecimalValue(request.getEstimatedAnnualIncome()))")
+    @Mapping(target = "estimatedNetWorth", expression = "java(roundUpBigDecimalValue(request.getEstimatedNetWorth()))")
     UserProfile requestToEntity(ProfileRequest request);
 
     @Mapping(target = "employmentStatus", expression = "java(checkSpouseEmploymentStatus(spouseProfile.getEmploymentStatus()))")
     SpouseProfile map(SpouseProfileDTO spouseProfile);
 
     ProfileRequest entityToRequest(UserProfile entity);
+
+    default BigDecimal roundUpBigDecimalValue(BigDecimal bigDecimalValue) {
+        if (!isNull(bigDecimalValue)) {
+            return bigDecimalValue.setScale(2, RoundingMode.HALF_EVEN);
+        }
+
+        return null;
+    }
 
     default EmploymentStatusEnum checkSpouseEmploymentStatus(EmploymentStatusEnum spouseEmploymentStatus) {
         if (isNull(spouseEmploymentStatus)) {
