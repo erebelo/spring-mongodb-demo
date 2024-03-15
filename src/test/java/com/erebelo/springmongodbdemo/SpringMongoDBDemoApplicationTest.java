00923e20bc90d3bc71f33b91fc521ada75d804c3
erebelo <eduldrebelo@gmail.com>
2024-03-15 00:16:42 -0300

    Enhance SpringMongoDBDemoApplication unit test

--- src/test/java/com/erebelo/springmongodbdemo/SpringMongoDBDemoApplicationTest.java
+++ src/test/java/com/erebelo/springmongodbdemo/SpringMongoDBDemoApplicationTest.java
@@ -1,10 +1,21 @@
 package com.erebelo.springmongodbdemo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+
+@ExtendWith(MockitoExtension.class)
 class SpringMongoDBDemoApplicationTest {
+
+    @Mock
+    private ConfigurableApplicationContext contextMock;
 
     @Test
     void contextLoads() {
@@ -14,6 +25,12 @@ class SpringMongoDBDemoApplicationTest {
 
     @Test
     void mainRunsSuccessfully() {
-        SpringMongoDBDemoApplication.main(new String[]{});
+        try (MockedStatic<SpringApplication> mockedStatic = mockStatic(SpringApplication.class)) {
+            mockedStatic.when(() -> SpringApplication.run(any(Class.class), any(String[].class))).thenReturn(contextMock);
+
+            SpringMongoDBDemoApplication.main(new String[]{});
+
+            mockedStatic.verify(() -> SpringApplication.run(SpringMongoDBDemoApplication.class, new String[]{}));
+        }
     }
 }
