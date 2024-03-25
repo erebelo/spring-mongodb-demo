4624c6c11074945f31009a48be40112593a4754c
erebelo <eduldrebelo@gmail.com>
2024-03-25 18:01:42 -0300

    Updating health check endpoint path

--- src/main/java/com/erebelo/springmongodbdemo/constants/BusinessConstants.java
+++ src/main/java/com/erebelo/springmongodbdemo/constants/BusinessConstants.java
@@ -9,7 +9,7 @@ public final class BusinessConstants {
     public static final String DEFAULT_CONNECTION_TIMEOUT = "3000";
     public static final String DEFAULT_SOCKET_TIMEOUT = "5000";
     public static final String MERGE_PATCH_MEDIA_TYPE = "application/merge-patch+json";
-    public static final String HEALTH_CHECK = "/healthcheck";
+    public static final String HEALTH_CHECK = "/health-check";
     public static final String PROFILE = "/profile";
     public static final String WIKIMEDIA = "/wikimedia";
     public static final String ARTICLES = "/articles";
