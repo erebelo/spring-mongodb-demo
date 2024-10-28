28dab5895ea59ebf67a2a9c0838024339e02de76
erebelo <eduldrebelo@gmail.com>
2024-10-28 18:43:30 -0300

    Removing unused Environment import

--- src/test/java/com/erebelo/springmongodbdemo/controller/ArticlesControllerTest.java
+++ src/test/java/com/erebelo/springmongodbdemo/controller/ArticlesControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
